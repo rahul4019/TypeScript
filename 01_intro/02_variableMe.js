ca84d10f9755cd8f4a022075a3ee17f4e9a4aaac
rahul4019 <rahulkashyap4019@gmail.com>
2023-05-10 11:48:35 +0530

    Number boolean and type inference

---
+++ 01_intro/02_variableMe.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = 'Hello Rahul';
+var myNum = 6;
+// myNum.toUpperCase(); => not allowed because myNum is a number
+greetings.toLowerCase();
+console.log(greetings);
+// Number
+var userId = 334455;
+// Boolean
+var isLoggedIn = false;
+console.log(isLoggedIn.valueOf());
---
+++ 01_intro/02_variableMe.ts
@@ -0,0 +1,20 @@
+let greetings: string = 'Hello Rahul';
+
+let myNum = 6;
+
+// myNum.toUpperCase(); => not allowed because myNum is a number
+
+greetings.toLowerCase();
+
+console.log(greetings);
+
+// Number
+
+let userId: number = 334455;
+
+// Boolean
+let isLoggedIn: boolean = false;
+
+console.log(isLoggedIn.valueOf());
+
+export {};
