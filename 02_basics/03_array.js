c902c4d172ff822906671968170848b3c438b727
rahul4019 <rahulkashyap4019@gmail.com>
2023-05-11 08:49:46 +0530

    Arrays in TypeScript

---
+++ 02_basics/03_array.js
@@ -0,0 +1,7 @@
+var superHeros = [];
+// const heroPowers: number[] = [];
+var heroPowers = []; // same working as previous, just different syntax
+var allUsers = [];
+superHeros.push('spiderman');
+heroPowers.push(2);
+allUsers.push({ name: 'rahul', isActive: false });
---
+++ 02_basics/03_array.ts
@@ -0,0 +1,24 @@
+const superHeros: string[] = [];
+// const heroPowers: number[] = [];
+const heroPowers: Array<number> = []; // same working as previous, just different syntax
+
+type User = {
+  name: String;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+
+// Array in array
+const MLModels: number[][] = [
+    [255,255,255],
+    []
+]
+
+superHeros.push('spiderman');
+heroPowers.push(2);
+
+allUsers.push({ name: 'rahul', isActive: false });
+
+
+export {}
