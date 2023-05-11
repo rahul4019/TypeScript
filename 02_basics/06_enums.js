3db33b736cb9e0fd47f7e2b7a973dea9cd2db1a7
rahul4019 <rahulkashyap4019@gmail.com>
2023-05-11 10:54:03 +0530

    Enum in TypeScript

---
+++ 02_basics/06_enums.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var SeatChoice;
+(function (SeatChoice) {
+    /* AISLE = 10,
+    MIDDLE = 1,
+    WINDOW,
+    FOURTH = 22,
+     */
+    SeatChoice["AISLE"] = "aisle";
+    SeatChoice["MIDDLE"] = "middle";
+    SeatChoice["WINDOW"] = "window";
+    SeatChoice["FOURTH"] = "fourth";
+})(SeatChoice || (SeatChoice = {}));
+var choice = SeatChoice.AISLE;
---
+++ 02_basics/06_enums.ts
@@ -0,0 +1,15 @@
+ enum SeatChoice {
+    /* AISLE = 10,
+    MIDDLE = 1,
+    WINDOW,
+    FOURTH = 22,
+     */
+    AISLE = 'aisle',
+    MIDDLE = 'middle',
+    WINDOW = 'window',
+    FOURTH = 'fourth',
+ }
+
+const choice = SeatChoice.AISLE
+
+export {}
